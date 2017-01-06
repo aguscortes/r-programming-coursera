afb704806ac107568b39fa7d2a1cbbc713264bce
Agus <agus@atadatos.es>
2017-01-06 18:08:41 +0100

    Programming Assignment 1 scripts (complete.R, corr.R and pollutanmean.R) were modified: comments added, still they continue being poorly coded and need to be improved.

--- programming_assignment_1/complete.R
+++ programming_assignment_1/complete.R
@@ -1,4 +1,9 @@
 complete <- function(directory, id = 1:332) {
+        ## 'directory' is a character vector of length 1 indicating
+        ## the location of the CSV files
+        
+        ## 'id' is an integer vector indicating the monitor ID numbers
+        ## to be used
         library(stringr)
         data <- vector()
         
